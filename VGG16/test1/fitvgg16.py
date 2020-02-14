587f6b0bc76009771cc5c2c208146a1427d78501
lasg <lasse973@gmail.com>
2020-02-14 11:22:42 +0100

    config done

--- VGG16/test1/fitvgg16.py
+++ VGG16/test1/fitvgg16.py
@@ -4,7 +4,6 @@ import tensorflow.keras as keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D,GlobalAveragePooling2D, Concatenate, Reshape,GlobalMaxPooling2D, Activation, Input
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-import IPython.display as display
 from PIL import Image
 import numpy as np
 import pandas
@@ -15,14 +14,25 @@ import math
 import sys
 import yaml
 
-modelNum = 3
-BS = 8 # Batch size
-image_height = 1280
-image_width = 2000
+stream = file('config.yml', 'r')    # 'document.yaml' contains a single YAML document.
+conf = yaml.safe_load(stream)
 
-train_df = pandas.read_csv("data/allTrain.csv")
-validate_df = pandas.read_csv("data/allVal.csv")
-test_df = pandas.read_csv("data/allTest.csv")
+
+train_path = conf['data'][0]['train_path']
+validate_path = conf['data'][1]['validate_path']
+
+image_dir = conf['data'][2]['image_dir']
+logFileName = conf['data'][3]['log_file']
+checkpointpath = conf['data'][4]['checkpoint_path']
+
+learning_rate = conf['model'][0]['learning_rate']
+
+image_height = conf['image_height']
+image_width = conf['image_width']
+batch_size = conf['batch_size']
+
+train_df = pandas.read_csv(train_path)
+validate_df = pandas.read_csv(validate_path)
 
 train_datagen = ImageDataGenerator(
         rescale=1./255,
@@ -35,43 +45,27 @@ val_datagen = ImageDataGenerator(
         )
 
 
-test_datagen = ImageDataGenerator(
-        rescale=1./255,
-        )
-
 train_generator = train_datagen.flow_from_dataframe(
         dataframe=train_df,
-        directory='',
+        directory=image_dir,
         x_col="filename",
         y_col='label',
         target_size=(image_height, image_width),
-        batch_size=BS,
+        batch_size=batch_size,
         shuffle=True,
         class_mode="raw"
         )
 
 val_generator = val_datagen.flow_from_dataframe(
         dataframe=validate_df,
-        directory='',
+        directory=image_dir,
         x_col="filename",
         y_col='label',
         target_size=(image_height, image_width),
-        batch_size=BS,
+        batch_size=batch_size,
         shuffle=True,
         class_mode="raw"
         )
-
-test_generator = test_datagen.flow_from_dataframe(
-        dataframe=test_df,
-        directory='',
-        x_col="filename",
-        y_col='label',
-        target_size=(image_height, image_width),
-        batch_size=1,
-        shuffle=False,
-        class_mode="raw"
-        )
-
 
 VGG16_MODEL = tf.keras.applications.VGG16(input_shape=(image_height,image_width,3),include_top=False,weights="imagenet")
 
@@ -83,18 +77,15 @@ for layer in VGG16_MODEL.layers:
 for l in model.layers:
     l.trainable=False
 
-model.add(GlobalAveragePooling2D())
-#model.add(Flatten())
+model.add(GlobalMaxPooling2D())
 
-#model.add(Dense(4096))
-
-model.add(Dense(512))
+#model.add(Dense(512))
 
 model.add(Dense(1))
-model.summary()
+#model.summary()
 
 
-optimize = keras.optimizers.RMSprop(learning_rate=0.0001)
+optimize = keras.optimizers.RMSprop(learning_rate=learning_rate)
 model.compile(optimizer=optimize,
               loss='MSE',
               metrics=['mse']
@@ -102,10 +93,16 @@ model.compile(optimizer=optimize,
 
 currentEpoch=0
 
-#!mkdir "checkpoints"
-filepath="pythonscripts/test2/checkpoints/model_"+str(modelNum)+"_checkpoint-"+str(image_height)+"x"+str(image_width)+"-{epoch:03d}-{val_loss:.5f}.hdf5"
+filepath=str(checkpointpath)+"model_"+str(modelNum)+"_checkpoint-"+str(image_height)+"x"+str(image_width)+"-{epoch:03d}-{val_loss:.5f}.hdf5"
+
 checkpoint = keras.callbacks.ModelCheckpoint(filepath, monitor='val_loss', verbose=0, save_best_only=True, save_weights_only=False, mode='min')
+
 earlyStop = keras.callbacks.EarlyStopping(monitor='val_loss', mode='min', patience=25, restore_best_weights=True,verbose=1)
+
+csvLog = keras.callbacks.callbacks.CSVLogger(logFileName, separator=',', append=True)
+
 callbacks_list = [checkpoint]
+
 history = model.fit(train_generator,validation_data=val_generator,verbose=1 , epochs=currentEpoch+(300-currentEpoch), steps_per_epoch=train_generator.n/train_generator.batch_size ,initial_epoch=currentEpoch, callbacks=callbacks_list)
+
 currentEpoch=len(history.history["loss"])
