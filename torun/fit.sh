5b424231c59d33a9a39b8ff9b1044a9b31e7b712
Your Name <you@example.com>
2020-03-05 16:39:51 +0100

    new

---
+++ torun/fit.sh
@@ -0,0 +1,7 @@
+rm ../../bachelor-data/checkpoints/*
+python resnet_baseline_dropout50.py
+rm ../../bachelor-data/checkpoints/*
+python resnet_baseline_3layerMLP_dropout50.py
+rm ../../bachelor-data/checkpoints/*
+python resnet_baseline_4layerMLP_dropout50.py
+rm ../../bachelor-data/checkpoints/*
