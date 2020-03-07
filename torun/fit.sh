cb26c3200c5b31eb9a6cb46cb02e73b411f53243
Your Name <you@example.com>
2020-03-07 15:30:03 +0100

    new

--- torun/fit.sh
+++ torun/fit.sh
@@ -1,6 +1,5 @@
 rm ../../bachelor-data/checkpoints/*
-python resnet_baseline_2layer_l2reg.py
+python resnet_baseline_3layer_l2reg.py
 rm ../../bachelor-data/checkpoints/*
-python resnet_baseline_1layer_l2reg.py
 rm ../../bachelor-data/checkpoints/*
 rm ../../bachelor-data/checkpoints/*
