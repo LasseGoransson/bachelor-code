854958d5886bbebf8122d083f6f923b4990ccf61
Your Name <you@example.com>
2020-04-21 12:59:02 +0200

    new

--- rerun_extend/run.sh
+++ rerun_extend/run.sh
@@ -1,4 +1,4 @@
-python resnet_extent_1layer_proj.py ; rm ~/bachelor-data/checkpoints/*  
-python resnet_extent_2layer_proj.py ; rm ~/bachelor-data/checkpoints/*
-python resnet_extent_3layer_proj.py ; rm ~/bachelor-data/checkpoints/*
-python resnet_extent_4layer_proj.py ; rm ~/bachelor-data/checkpoints/*
+python resnet_extent_1layer_proj_l2reg.py ; rm ~/bachelor-data/checkpoints/* 
+python resnet_extent_2layer_proj_l2reg.py ; rm ~/bachelor-data/checkpoints/* 
+python resnet_extent_3layer_proj_l2reg.py ; rm ~/bachelor-data/checkpoints/* 
+python resnet_extent_4layer_proj_l2reg.py ; rm ~/bachelor-data/checkpoints/* 
